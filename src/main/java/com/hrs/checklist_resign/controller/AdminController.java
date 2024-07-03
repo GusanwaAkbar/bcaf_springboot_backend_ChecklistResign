043b16e6d682d0bb55c0c0823d5546e02c037ae5
GusawnaAkbar <gusanwaakbar@gmail.com>
2024-07-03 13:54:35 +0700

    admin management

--- src/main/java/com/hrs/checklist_resign/controller/AdminController.java
+++ src/main/java/com/hrs/checklist_resign/controller/AdminController.java
@@ -1,29 +1,49 @@
-//package com.hrs.checklist_resign.controller;
-//
-//
-//import com.hrs.checklist_resign.Model.User;
-//import com.hrs.checklist_resign.response.ApiResponse;
-//import com.hrs.checklist_resign.service.AdminService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.*;
-//import  com.hrs.checklist_resign.dto.RoleUpdateRequest;
-//
-//import jakarta.validation.Valid;
-//
-//@RestController
-//@RequestMapping("/api/admin")
-//public class AdminController {
-//
-//    @Autowired
-//    private AdminService adminService;
-//
-//    @PreAuthorize("hasRole('ADMIN')")
-//    @PutMapping("/users/{username}/roles")
-//    public ResponseEntity<ApiResponse<User>> updateUserRoles(@PathVariable String username, @Valid @RequestBody RoleUpdateRequest roleUpdateRequest) {
-//        ApiResponse<User> response = adminService.updateUserRoles(username, roleUpdateRequest.getRoles());
-//        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
-//    }
-//}
+package com.hrs.checklist_resign.controller;
+
+
+import com.hrs.checklist_resign.Model.ApprovalAtasan;
+import com.hrs.checklist_resign.Model.User;
+import com.hrs.checklist_resign.dto.UserResponseDTO;
+import com.hrs.checklist_resign.repository.UserRepository;
+import com.hrs.checklist_resign.response.ApiResponse;
+import com.hrs.checklist_resign.service.AdminService;
+import com.hrs.checklist_resign.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import  com.hrs.checklist_resign.dto.RoleUpdateRequest;
+
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AdminService adminService;
+
+    @GetMapping("/non-user-authorities")
+    public ResponseEntity<ApiResponse<List<UserResponseDTO>>> getUsersWithNonUserAuthorities() {
+        List<UserResponseDTO> listUserDTO = adminService.findUsersWithRolesNotContainingV2("USER");
+
+        if (!listUserDTO.isEmpty()) {
+            ApiResponse<List<UserResponseDTO>> response = new ApiResponse<>(listUserDTO, true, "Record fetched successfully", HttpStatus.OK.value());
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            ApiResponse<List<UserResponseDTO>> response = new ApiResponse<>(false, "Record not found", HttpStatus.NOT_FOUND.value(), "ApprovalAtasan not found");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+}
--- src/main/java/com/hrs/checklist_resign/repository/UserRepository.java
+++ src/main/java/com/hrs/checklist_resign/repository/UserRepository.java
@@ -1,13 +1,28 @@
 package com.hrs.checklist_resign.repository;
 
 import com.hrs.checklist_resign.Model.User;
+import com.hrs.checklist_resign.dto.UserResponseDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.roles NOT LIKE %:role%")
+    List<User> findUsersWithRolesNotContaining(@Param("role") String role);
+
+//    @Query("SELECT new com.hrs.checklist_resign.dto.UserResponseDTO(u.username, u.userDetails, u.authorities, u.rolesList) " +
+//            "FROM User u WHERE u.roles NOT LIKE %:role%")
+//    List<UserResponseDTO> findUsersWithRolesNotContainingV2(@Param("role") String role);
+
+    @Query("SELECT new com.hrs.checklist_resign.dto.UserResponseDTO(u.username, u.userDetails, u.roles) " +
+            "FROM User u WHERE u.roles NOT LIKE %:role%")
+    List<UserResponseDTO> findUsersWithRolesNotContainingV2(@Param("role") String role);
 
 }
